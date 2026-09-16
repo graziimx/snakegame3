83e27def82b86709f421cadcfeef1e302c067910
graziimx <alexia.graziely.alves@escola.pr.gov.br>
2026-09-16 09:09:23 -0300

    Create GameConstants.java

---
+++ GameConstants.java
@@ -0,0 +1,15 @@
+public final class Gameconstants {
+
+    private Gameconstants (){
+        
+        public static final String GAME_TITLE = "Snake Game";
+
+        public static final int TILE = 24; //formato de telha 
+
+        public static final int COLS = 20; //colunas
+        public static final int ROWS =20; //linhas
+
+        public static final int WIDTH = TILE * COLS;
+        public static final int HEIGHT = TILE * ROWS;
+    }
+}
